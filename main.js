14ea3945b15cc106d58718d79d96e83788c4c480
Moises Valdez <moisesvaldez075@gmail.com>
2020-02-27 10:54:44 -0800

    added a high order array method, and evenlistender

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const main = document.getElementById('main');
-const user = document.getElementById('add-user');
+const addUser = document.getElementById('add-user');
 const double = document.getElementById('double');
 const show_millionair = document.getElementById('show-millionair');
 const sort = document.getElementById('sort');
 const calculate_wealth = document.getElementById('calculate-wealth');
 
 let data = [];
-
-
 
 // fetch random user and add money
 async function getRandomUser() {
@@ -30,4 +28,30 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
     data.push(obj);
+
+
+    updateDOM();
 }
+
+// update DOM()
+function updateDOM(proviedDate = data) {
+    // clear main div
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+    proviedDate.forEach(item => {
+        const div = document.createElement('div');
+        div.classList.add('person');
+
+        div.innerHTML = `<strong>${item.name}</strong> $${formatMoney(item.money)}`
+
+        main.appendChild(div)
+    });
+}
+
+// function money
+function formatMoney(number) {
+    return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// event listener
+addUser.addEventListener('click', getRandomUser);
